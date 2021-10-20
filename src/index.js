ef95852440b5e195b5bf6c1092bd10797dcf4880
dahelg <hdrews87@gmail.com>
2021-10-20 17:02:49 +0200

    try less

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+import './teststyle.less';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hellooo', 'webpack', '!!!!!!!'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', '!'], ' ');
   element.classList.add('hello');
+  element.classList.add('helloo');
 
   return element;
 }
